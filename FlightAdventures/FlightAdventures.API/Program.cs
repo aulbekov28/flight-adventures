8e826a43f261b25f6a4b036b800c2435f1cfeaf6
Askar <askar.aulbekov>
2023-04-03 23:11:11 +0600

    add swagger. seed data generator

--- FlightAdventures/FlightAdventures.API/Program.cs
+++ FlightAdventures/FlightAdventures.API/Program.cs
@@ -3,6 +3,7 @@ using FlightAdventures.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Host.UseSerilog((hostContext, services, configuration) => {
     configuration.WriteTo.Console();
 });
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(GetFlightsQuery).Assembly));
 
@@ -21,6 +25,25 @@ builder.Services
     );
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    // Initialise and seed database
+    using var scope = app.Services.CreateScope();
+    var initializer = scope.ServiceProvider.GetRequiredService<FlightDbContextInitializer>();
+    await initializer.InitialiseAsync();
+    await initializer.SeedAsync();
+}
+else
+{
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapGet("/", () => "Hello World!");
 
